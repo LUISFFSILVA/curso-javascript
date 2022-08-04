c383eeaa1997fecf1b86dd49cea73905d804228d
Luis Fernando <luis.silva1102@gmail.com>
2022-08-04 09:46:14 -0300

    atualizado para rodar em pagina web

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,10 @@
-const {soma} = require('./somar');
+/*Este modo para rodar no NODE.JS
+const { soma } = require("./somar");
 
-const teste = soma(9,5)
+const teste = soma(9, 5);
+console.log(teste);
+*/
+
+//Este modo para rodar no NAVEGADOR
+const teste = soma(9, 5);
 console.log(teste);
--- somar.js
+++ somar.js
@@ -1,7 +1,11 @@
-
-//teste git
+//Este modo para rodar no NODE.JS
+/*
 module.exports.soma = function(a,b){
     return a + b;
 }
+*/
 
-
+//Este modo para utilizar no navegador
+function soma(a, b) {
+  return a + b;
+}
